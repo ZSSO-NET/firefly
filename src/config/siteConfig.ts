d09df8eab10e8ec9da76768dd33e7a9ef8145110
ZSSO <1867084@qq.com>
2026-06-08 19:17:04 +0800

    src/config/siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -75,7 +75,7 @@ export const siteConfig: SiteConfig = {
 		// 4. 网络图片: { type: "url", value: "https://example.com/logo.png", alt: "Logo" }
 		logo: {
 			type: "image",
-			value: "https://u.fukit.cn/n7PJhqyoU",
+			value: "assets/images/firefly.png",
 			alt: "🍀",
 		},
 		// 导航栏标题
